2c09a4c7ccaa53183702e22cfe7d299334e0169e
Gwen Merida <gd.merida@gmail.com>
2019-11-02 10:34:39 +0900

    INIT commit

---
+++ src/components/Bags.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-content>
+    <div class="text-center my-12">
+      <span class="font-weight-black" style="font-size: 3em; font-family: 'Amatic SC', cursive;"><u>We are giving a bag of your choice from our list</u></span><br/>
+      <span class="font-weight-light">when you donate or sponsor the printing of our Christian magazine ( Buddies of Christ-Tropa ni Cristo )</span><br/>
+      <span class="font-weight-light">Minimum donation is  $1500.00 to be eligible for the gift.</span>
+    </div>
+    <v-row>
+      <v-col md="6" sm="12">
+        <v-carousel style="border: grey 2px solid">
+          <v-carousel-item
+                  v-for="(item,i) in items"
+                  :key="i"
+                  :src="item"
+                  reverse-transition="fade-transition"
+                  transition="fade-transition"
+          ></v-carousel-item>
+        </v-carousel>
+      </v-col>
+      <v-col md="6" sm="12">
+        <v-card class="pa-5">
+          <v-container>
+            <v-row>
+              <v-col col="12" sm="12">
+                <span class="font-weight-black">Sole Propretorship Registration Number: </span>
+              </v-col>
+              <v-col col="12" sm="12">
+                <span class="font-weight-light">FM0728978</span>
+              </v-col>
+              <v-col col="12" sm="12">
+                <span class="font-weight-black">Name of Sole Proprietorship: </span>
+              </v-col>
+              <v-col col="12" sm="12">
+                <span class="font-weight-light">Youth For Truth</span>
+              </v-col>
+              <v-col col="12" sm="12">
+                <span class="font-weight-black">Registration Date: </span>
+              </v-col>
+              <v-col col="12" sm="12">
+                <span class="font-weight-light">September 21, 2017</span>
+              </v-col>
+              <v-col col="12" sm="12">
+                <span class="font-weight-black">Business Start Date: </span>
+              </v-col>
+              <v-col col="12" sm="12">
+                <span class="font-weight-light">Jan 01, 2018</span>
+              </v-col>
+              <v-col col="12" sm="12">
+                <span class="font-weight-black">Business Address: </span>
+              </v-col>
+              <v-col col="12" sm="12">
+                <span class="font-weight-light">603 - 17th 17th ST W NORTH VANCOUVER BC V7M 1V5</span>
+              </v-col>
+              <v-col col="12" sm="12">
+                <span class="font-weight-black">Bank Account: </span>
+              </v-col>
+              <v-col col="12" sm="12">
+                <span class="font-weight-light">525 138 4 (Checking Account)</span>
+              </v-col>
+              <v-col col="12" sm="12">
+                <span class="font-weight-black">Facebook Page: </span>
+              </v-col>
+              <v-col col="12" sm="12">
+                <a href="https://www.facebook.com/bagswithamission/" target="_blank">
+                  <span>https://www.facebook.com/bagswithamission/</span>
+                </a>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-content>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                items: [
+                    'https://bagswithamission.s3.amazonaws.com/Birkin10%5B1111%5D.jpg',
+                    'https://bagswithamission.s3.amazonaws.com/bag+cause2%5B1113%5D.jpg',
+                    'https://bagswithamission.s3.amazonaws.com/Bags+w+Cause%5B1114%5D.jpg'
+                ]
+            }
+        },
+    }
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-content class="background">
+    <div class="home-top-video">
+      <div class="d-flex flex-column home-text text-center">
+        <span style="font-size: 6em; font-family: 'Dosis', sans-serif; ">Bags with a Mission</span>
+      </div>
+    </div>
+  </v-content>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            bgImg: 'https://bagswithamission.s3.amazonaws.com/bg.jpeg'
+        }),
+    };
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Dosis&display=swap');
+  @import url('https://fonts.googleapis.com/css?family=Amatic+SC&display=swap');
+
+  @media only screen and (max-width: 768px) {
+    /* For mobile phones: */
+    .background {
+      height: 100vh;
+      background: url('https://bagswithamission.s3.amazonaws.com/bg.jpeg') no-repeat center;
+    }
+  }
+
+  @media only screen and (min-width: 768px) {
+    /* For desktop: */
+    .background {
+      height: 50vh;
+      background: url('https://bagswithamission.s3.amazonaws.com/bg.jpeg') no-repeat center;
+    }
+  }
+
+  .home-top-video:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    z-index: 1;
+    background: linear-gradient(
+            to right,
+            rgba(0, 0, 0, 0.5),
+            rgba(99, 50, 110, 0.7)
+    );
+  }
+
+  .home-top-video {
+    left: 0;
+    top: 0;
+    height: 50vh;
+    width: 100%;
+    overflow: hidden;
+    z-index: -1;
+  }
+
+  .home-text {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    color: #fff;
+    z-index: 10;
+  }
+</style>
